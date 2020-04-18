e5e047267ceeea9161b0676fe58510f663accaad
Nasir Osman <nmosman@ucalgary.ca>
2020-04-18 17:32:45 -0600

    added UI components

--- src/App.js
+++ src/App.js
@@ -2,25 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button'
+import SplitButton from 'react-bootstrap/SplitButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+	
+
 function App() {
   return (
     <div className="App">
      <h2> Indistinguishability game </h2>
       <header className="App-header">
       
-      <Button> Start Game </Button>
-      <Dropdown>
+
+      	<Router>
+     	 <Link to='/GamePage'>
+      	<Button> Start Game </Button>
+
+		</Link>
+
+
+
+		  <Switch>
+          <Route path="/GamePage">
+            <GamePage />
+            </Route>
+            </Switch>
+			</Router>
+  <p> Select your encrpytion scheme</p>
+      <Dropdown title='Encrpytion Scheme'>
   <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
+    Encrpytion Scheme
   </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+
+  <Dropdown.Menu >
+    <Dropdown.Item href="#/action-1">OTP with 1 digit (no carry)</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">1 digit with carry</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Custom</Dropdown.Item>
   </Dropdown.Menu>
 </Dropdown>
+
+
       </header>
       <body>
       Test
@@ -32,3 +62,68 @@ function App() {
 }
 
 export default App;
+class GamePage extends React.Component{
+ componentWillMount() {
+    document.body.classList.add('page-not-found')
+  }
+  
+  componentWillUnmount() {
+    document.body.classList.remove('page-not-found');
+  }
+		render(){
+ 
+		
+		return(
+		<p> Game time! </p>
+		);
+	}}
+
+class Home extends React.Component{
+render(){
+	return(
+
+ <div className="App">
+     <h2> Indistinguishability game </h2>
+      <header className="App-header">
+      
+
+      	<Router>
+     	 <Link to='/GamePage'>
+      	<Button> Start Game </Button>
+
+		</Link>
+
+
+
+		  <Switch>
+          <Route path="/GamePage">
+            <GamePage />
+            </Route>
+            </Switch>
+			</Router>
+
+      <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Dropdown Button
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+
+
+      </header>
+      <body>
+      Test
+
+      
+      </body>
+    </div>
+	);
+}
+
+
+}
---
+++ src/Components/UI.js
@@ -0,0 +1,14 @@
+class GamePage extends React.Component{
+	componentWillMount(){
+
+	}
+
+	componentWillUnmount(){
+
+	}
+
+	render(){
+		return
+		<h1> Game time! </h1>
+	}
+}
