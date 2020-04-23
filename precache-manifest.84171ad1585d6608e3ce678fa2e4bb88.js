e472388ddc115fbde682f2690f110299beed3398
Nasir Osman <nmosman@ucalgary.ca>
2020-04-23 14:38:19 -0600

    Updates

---
+++ precache-manifest.84171ad1585d6608e3ce678fa2e4bb88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b380119353f7af5cab3981cdbaf5fd",
+    "url": "/otp_game/index.html"
+  },
+  {
+    "revision": "5f9b9c1b7e41c0bc819b",
+    "url": "/otp_game/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "13b4ff57bb203d556c97",
+    "url": "/otp_game/static/css/main.a2837438.chunk.css"
+  },
+  {
+    "revision": "5f9b9c1b7e41c0bc819b",
+    "url": "/otp_game/static/js/2.6e2891f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/otp_game/static/js/2.6e2891f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b4ff57bb203d556c97",
+    "url": "/otp_game/static/js/main.eb7f82a0.chunk.js"
+  },
+  {
+    "revision": "528bc708e9ec8aceed06",
+    "url": "/otp_game/static/js/runtime-main.efce95b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/otp_game/static/media/logo.5d5d9eef.svg"
+  }
+]);
